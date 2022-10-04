833abbd420c83e52eb75decbff23f7fa4af795f0
Milos <milosandric96@gmail.com>
2022-10-04 12:04:51 +0200

    Zavrsen post

--- src/pages/ObrisiPost.jsx
+++ src/pages/ObrisiPost.jsx
@@ -1,8 +1,71 @@
 import React from 'react';
+import { useState } from 'react';
 
 function ObrisiPost() {
+    const [naslovEnabled, setNaslovEnabled] = useState(true);
+    const [tekstEnabled, setTekstEnabled] = useState(true);
+    const [lajkoviEnabled, setLajkoviEnabled] = useState(true);
+    const [datumEnabled, setDatumEnabled] = useState(true);
+    var post = { "naslov":"","tekst":"", "lajkovi":0,"datum_postavljanja":"" }
+
+    const naslovChange = (event) => {
+       post["naslov"] = event.target.value
+    }
+
+    const checkNaslovEnable = (event) => {
+        setNaslovEnabled(!naslovEnabled) 
+    }
+
+    const checkTekstEnabled = (event) => {
+        setTekstEnabled(!tekstEnabled) 
+    }
+
+    const checkLajkoviEnabled = (event) => {
+        setLajkoviEnabled(!lajkoviEnabled) 
+    }
+
+    const checkDatumEnabled = (event) => {
+        setDatumEnabled(!datumEnabled) 
+    }
+
+    const tekstChange = (event) => {
+        post["tekst"] = event.target.value
+    }
+
+    const lajkoviChange = (event) => {
+        post["lajkovi"] = event.target.value
+    }
+
+    const datumChange = (event) => {
+        post["datum"] = event.target.value
+    }
+
+    function Klik(){
+        post["datum_postavljanja"] = Date()
+        alert(JSON.stringify(post))
+    }
+
     return (<>
-        <h1>Obrisi</h1>
+    <p class="instrukcije">Instrukcije za koriscenje alata: aktivirati filtere i uneti po kom kriterijumu da se brisu svi elementi u bazi koji ispunjavaju dati uslov.</p>
+    <form class="okolina">
+        <input type="checkbox" onChange={checkNaslovEnable}/>
+        <label> Naslov filter</label>
+        <input type="text" onChange={naslovChange} disabled={naslovEnabled}/>
+
+        <input type="checkbox" onChange={checkTekstEnabled}/>
+        <label>Tekst filter</label>
+        <input type="text" onChange={tekstChange} disabled={tekstEnabled}/>
+
+        <input type="checkbox" onChange={checkLajkoviEnabled}/>
+        <label>Lajkovi filter</label>
+        <input type="number" onChange={lajkoviChange} disabled={lajkoviEnabled}/>
+
+        <input type="checkbox" onChange={checkDatumEnabled}/>
+        <label>Datum filter</label>
+        <input type="date" onChange={datumChange} disabled={datumEnabled}/>
+    
+        <input type="submit" value="Submit" onClick={Klik}/>
+    </form>
     </>);
 }
 
