d5fd462ac5a8ca845365dcb19ecf40d58b9ec2fb
Milos <milosandric96@gmail.com>
2022-11-08 12:02:30 +0100

    Ulepsane tabele

--- src/Table.js
+++ src/Table.js
@@ -1,10 +1,23 @@
+function ulepsaj(string) {
+  return (string.charAt(0).toUpperCase() + string.slice(1)).replaceAll("_"," ");
+}
+
+function ulepsajDatum(string,key) {
+  if (!key.includes("dat"))
+  {
+    return string;
+  }
+  var novi=new Date(string);
+  return novi.toDateString();
+}
+
 export default function Table({theadData, tbodyData}) {
     return (
-      <table>
+      <table id="tabela">
           <thead>
              <tr>
               {theadData.map(heading => {
-                return <th key={heading}>{heading}</th>
+                return <th key={heading}>{ulepsaj(heading)}</th>
               })}
               <th colspan="2">Action</th>
             </tr>
@@ -13,7 +26,7 @@ export default function Table({theadData, tbodyData}) {
               {tbodyData.map((row, index) => {
                   return <tr key={index}>
                       {theadData.map((key, index) => {
-                           return <td key={row[key]}>{row[key]}</td>
+                           return <td key={row[key]}>{ulepsajDatum(row[key],key)}</td>
                       })}
                     <td><a href="">Obrisi</a></td>
                     <td><a href="">Izmeni</a></td>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,19 @@
 import React from 'react';
+import data from "../data.json"
+import Table from '../Table';
 
 function Home() {
-    return (<>
-        <h1>User</h1>
 
-<table id="tabela">
-  <tr>
-    <th>Id</th>
-    <th>Ime</th>
-    <th>Prezime</th>
-    <th>Username</th>
-    <th>Password</th>
-    <th>Aktivan</th>
-    <th>Role</th>
-    <th>Datum rodjenja</th>
-    <th>Datum pravljenja naloga</th>
-    <th colspan="2">Action</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-    <td>Germany</td>
-    <td>Germany</td>
-    <td>Germany</td>
-    <td>Germany</td>
-    <td>Germany</td>
-    <td>Germany</td>
-    <td><a href="">Obrisi</a></td>
-    <td><a href="">Izmeni</a></td>
-  </tr>
-</table>
+  const getHeadings = () => {
+
+    return Object.keys(data[0]);
+}
+
+
+    return (<>
+      <h1>User</h1>
+
+      <Table theadData={getHeadings()} tbodyData={data}/>
 
 
 <h1>Post</h1>
